cfa0dd07cb82f2d79d19d2e10dab6945389ccb11
KE Gustafsson <ke.gustafsson@gmail.com>
2021-01-08 19:10:37 +0200

    for testing

--- index.js
+++ index.js
@@ -22,122 +22,115 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-'use strict';
+"use strict";
 process.chdir(__dirname);
 
-const onvif = require('./lib/node-onvif.js');
-const WebSocketServer = require('websocket').server;
+const onvif = require("./lib/node-onvif.js");
+const WebSocketServer = require("websocket").server;
 
 module.exports = function createPlugin(app) {
   const plugin = {};
-  plugin.id = 'signalk-node-onvif';
-  plugin.name = 'Signal K node-onvif';
-  plugin.description = 'Signal K node-onvif ';
+  plugin.id = "signalk-node-onvif";
+  plugin.name = "Signal K node-onvif";
+  plugin.description = "Signal K node-onvif ";
   var dummy = null;
   const setStatus = app.setPluginStatus || app.setProviderStatus;
 
-  var wsServer = {};
-  var http = require('http');
-  var fs = require('fs');
+  var http = require("http");
+  var fs = require("fs");
   var port = 8880;
-
+  let wsServer;
   let http_server;
 
   plugin.start = function (options, restartPlugin) {
     http_server = http.createServer(httpServerRequest);
     http_server.listen(port, function () {
-      console.log("Listening on port " + port);
+      console.log("Node-onvif HTTP server listening on port " + port);
     });
     wsServer = new WebSocketServer({
       httpServer: http_server,
     });
-    wsServer.on('request', wsServerRequest);
+    wsServer.on("request", wsServerRequest);
 
-    app.debug('Plugin started');
+    app.debug("Plugin started");
   };
 
   plugin.stop = function stop() {
-    if (typeof (wsServer.shutDown) == "function") {
-      wsServer.shutDown();
-      app.debug('Stopped');
-    }
     if (http_server) {
+      wsServer.shutDown();
       http_server.close(() => {
-        console.log('closed')
-      })
+        console.log("Node-onvif HTTP server closed");
+      });
     }
   };
 
-
-
   plugin.schema = {
-    type: 'object',
+    type: "object",
     properties: {
       dummy: {
-        type: 'integer',
+        type: "integer",
         default: 1,
-        title: 'dummy',
-      }
+        title: "dummy",
+      },
     },
   };
 
   function httpServerRequest(req, res) {
-    var path = req.url.replace(/\?.*$/, '');
+    var path = req.url.replace(/\?.*$/, "");
     if (path.match(/\.{2,}/) || path.match(/[^a-zA-Z\d\_\-\.\/]/)) {
       httpServerResponse404(req.url, res);
       return;
     }
-    if (path === '/') {
-      path = '/onvif.html';
+    if (path === "/") {
+      path = "/index.html";
     }
-    var fpath = './html' + path;
-    fs.readFile(fpath, 'utf-8', function (err, data) {
+    var fpath = "." + path;
+    fs.readFile(fpath, "utf-8", function (err, data) {
       if (err) {
         httpServerResponse404(req.url, res);
         return;
       } else {
         var ctype = getContentType(fpath);
-        res.writeHead(200, { 'Content-Type': ctype });
+        res.writeHead(200, { "Content-Type": ctype });
         res.write(data);
         res.end();
-        console.log('HTTP : 200 OK : ' + req.url);
       }
     });
   }
 
   function getContentType(fpath) {
-    var ext = fpath.split('.').pop().toLowerCase();
+    var ext = fpath.split(".").pop().toLowerCase();
     if (ext.match(/^(html|htm)$/)) {
-      return 'text/html';
+      return "text/html";
     } else if (ext.match(/^(jpeg|jpg)$/)) {
-      return 'image/jpeg';
+      return "image/jpeg";
     } else if (ext.match(/^(png|gif)$/)) {
-      return 'image/' + ext;
-    } else if (ext === 'css') {
-      return 'text/css';
-    } else if (ext === 'js') {
-      return 'text/javascript';
-    } else if (ext === 'woff2') {
-      return 'application/font-woff';
-    } else if (ext === 'woff') {
-      return 'application/font-woff';
-    } else if (ext === 'ttf') {
-      return 'application/font-ttf';
-    } else if (ext === 'svg') {
-      return 'image/svg+xml';
-    } else if (ext === 'eot') {
-      return 'application/vnd.ms-fontobject';
-    } else if (ext === 'oft') {
-      return 'application/x-font-otf';
+      return "image/" + ext;
+    } else if (ext === "css") {
+      return "text/css";
+    } else if (ext === "js") {
+      return "text/javascript";
+    } else if (ext === "woff2") {
+      return "application/font-woff";
+    } else if (ext === "woff") {
+      return "application/font-woff";
+    } else if (ext === "ttf") {
+      return "application/font-ttf";
+    } else if (ext === "svg") {
+      return "image/svg+xml";
+    } else if (ext === "eot") {
+      return "application/vnd.ms-fontobject";
+    } else if (ext === "oft") {
+      return "application/x-font-otf";
     } else {
-      return 'application/octet-stream';
+      return "application/octet-stream";
     }
   }
 
   function httpServerResponse404(url, res) {
-    res.write('404 Not Found: ' + url);
+    res.write("404 Not Found: " + url);
     res.end();
-    console.log('HTTP : 404 Not Found : ' + url);
+    console.log("HTTP : 404 Not Found : " + url);
   }
 
   var client_list = [];
@@ -145,67 +138,71 @@ module.exports = function createPlugin(app) {
   function wsServerRequest(request) {
     var conn = request.accept(null, request.origin);
     conn.on("message", function (message) {
-      if (message.type !== 'utf8') {
+      if (message.type !== "utf8") {
         return;
       }
       var data = JSON.parse(message.utf8Data);
-      var method = data['method'];
-      var params = data['params'];
-      if (method === 'startDiscovery') {
+      var method = data["method"];
+      var params = data["params"];
+      if (method === "startDiscovery") {
         startDiscovery(conn);
-      } else if (method === 'connect') {
+      } else if (method === "connect") {
         connect(conn, params);
-      } else if (method === 'fetchSnapshot') {
+      } else if (method === "fetchSnapshot") {
         fetchSnapshot(conn, params);
-      } else if (method === 'ptzMove') {
+      } else if (method === "ptzMove") {
         ptzMove(conn, params);
-      } else if (method === 'ptzStop') {
+      } else if (method === "ptzStop") {
         ptzStop(conn, params);
-      } else if (method === 'ptzHome') {
+      } else if (method === "ptzHome") {
         ptzHome(conn, params);
       }
     });
 
-    conn.on("close", function (message) {
-
-    });
+    conn.on("close", function (message) {});
     conn.on("error", function (error) {
       console.log(error);
     });
-  };
+  }
 
   var devices = {};
   function startDiscovery(conn) {
     devices = {};
     let names = {};
-    onvif.startProbe().then((device_list) => {
-      device_list.forEach((device) => {
-        let odevice = new onvif.OnvifDevice({
-          xaddr: device.xaddrs[0]
+    onvif
+      .startProbe()
+      .then((device_list) => {
+        device_list.forEach((device) => {
+          let odevice = new onvif.OnvifDevice({
+            xaddr: device.xaddrs[0],
+          });
+          let addr = odevice.address;
+          devices[addr] = odevice;
+          names[addr] = device.name;
         });
-        let addr = odevice.address;
-        devices[addr] = odevice;
-        names[addr] = device.name;
-      });
-      var devs = {};
-      for (var addr in devices) {
-        devs[addr] = {
-          name: names[addr],
-          address: addr
+        var devs = {};
+        for (var addr in devices) {
+          devs[addr] = {
+            name: names[addr],
+            address: addr,
+          };
         }
-      }
-      let res = { 'id': 'startDiscovery', 'result': devs };
-      conn.send(JSON.stringify(res));
-    }).catch((error) => {
-      let res = { 'id': 'connect', 'error': error.message };
-      conn.send(JSON.stringify(res));
-    });
+        let res = { id: "startDiscovery", result: devs };
+        conn.send(JSON.stringify(res));
+      })
+      .catch((error) => {
+        let res = { id: "connect", error: error.message };
+        conn.send(JSON.stringify(res));
+      });
   }
 
   function connect(conn, params) {
     var device = devices[params.address];
     if (!device) {
-      var res = { 'id': 'connect', 'error': 'The specified device is not found: ' + params.address };
+      var res = {
+        id: "connect",
+        error: "The specified device is not found: " + params.address,
+      };
       conn.send(JSON.stringify(res));
       return;
     }
@@ -213,11 +210,11 @@ module.exports = function createPlugin(app) {
       device.setAuth(params.user, params.pass);
     }
     device.init((error, result) => {
-      var res = { 'id': 'connect' };
+      var res = { id: "connect" };
       if (error) {
-        res['error'] = error.toString();
+        res["error"] = error.toString();
       } else {
-        res['result'] = result;
+        res["result"] = result;
       }
       conn.send(JSON.stringify(res));
     });
@@ -226,21 +223,23 @@ module.exports = function createPlugin(app) {
   function fetchSnapshot(conn, params) {
     var device = devices[params.address];
     if (!device) {
-      var res = { 'id': 'fetchSnapshot', 'error': 'The specified device is not found: ' + params.address };
+      var res = {
+        id: "fetchSnapshot",
+        error: "The specified device is not found: " + params.address,
+      };
       conn.send(JSON.stringify(res));
       return;
     }
     device.fetchSnapshot((error, result) => {
-      var res = { 'id': 'fetchSnapshot' };
+      var res = { id: "fetchSnapshot" };
       if (error) {
-        res['error'] = error.toString();
+        res["error"] = error.toString();
       } else {
-        var ct = result['headers']['content-type'];
-        var buffer = result['body'];
-        var b64 = buffer.toString('base64');
-        var uri = 'data:' + ct + ';base64,' + b64;
-        res['result'] = uri;
-
+        var ct = result["headers"]["content-type"];
+        var buffer = result["body"];
+        var b64 = buffer.toString("base64");
+        var uri = "data:" + ct + ";base64," + b64;
+        res["result"] = uri;
       }
       conn.send(JSON.stringify(res));
     });
@@ -249,16 +248,19 @@ module.exports = function createPlugin(app) {
   function ptzMove(conn, params) {
     var device = devices[params.address];
     if (!device) {
-      var res = { 'id': 'ptzMove', 'error': 'The specified device is not found: ' + params.address };
+      var res = {
+        id: "ptzMove",
+        error: "The specified device is not found: " + params.address,
+      };
       conn.send(JSON.stringify(res));
       return;
     }
     device.ptzMove(params, (error) => {
-      var res = { 'id': 'ptzMove' };
+      var res = { id: "ptzMove" };
       if (error) {
-        res['error'] = error.toString();
+        res["error"] = error.toString();
       } else {
-        res['result'] = true;
+        res["result"] = true;
       }
       conn.send(JSON.stringify(res));
     });
@@ -267,16 +269,19 @@ module.exports = function createPlugin(app) {
   function ptzStop(conn, params) {
     var device = devices[params.address];
     if (!device) {
-      var res = { 'id': 'ptzStop', 'error': 'The specified device is not found: ' + params.address };
+      var res = {
+        id: "ptzStop",
+        error: "The specified device is not found: " + params.address,
+      };
       conn.send(JSON.stringify(res));
       return;
     }
     device.ptzStop((error) => {
-      var res = { 'id': 'ptzStop' };
+      var res = { id: "ptzStop" };
       if (error) {
-        res['error'] = error.toString();
+        res["error"] = error.toString();
       } else {
-        res['result'] = true;
+        res["result"] = true;
       }
       conn.send(JSON.stringify(res));
     });
@@ -285,12 +290,18 @@ module.exports = function createPlugin(app) {
   function ptzHome(conn, params) {
     var device = devices[params.address];
     if (!device) {
-      var res = { 'id': 'ptzMove', 'error': 'The specified device is not found: ' + params.address };
+      var res = {
+        id: "ptzMove",
+        error: "The specified device is not found: " + params.address,
+      };
       conn.send(JSON.stringify(res));
       return;
     }
     if (!device.services.ptz) {
-      var res = { 'id': 'ptzHome', 'error': 'The specified device does not support PTZ.' };
+      var res = {
+        id: "ptzHome",
+        error: "The specified device does not support PTZ.",
+      };
       conn.send(JSON.stringify(res));
       return;
     }
@@ -298,15 +309,15 @@ module.exports = function createPlugin(app) {
     var ptz = device.services.ptz;
     var profile = device.getCurrentProfile();
     var params = {
-      'ProfileToken': profile['token'],
-      'Speed': 1
+      ProfileToken: profile["token"],
+      Speed: 1,
     };
     ptz.gotoHomePosition(params, (error, result) => {
-      var res = { 'id': 'ptzMove' };
+      var res = { id: "ptzMove" };
       if (error) {
-        res['error'] = error.toString();
+        res["error"] = error.toString();
       } else {
-        res['result'] = true;
+        res["result"] = true;
       }
       conn.send(JSON.stringify(res));
     });
